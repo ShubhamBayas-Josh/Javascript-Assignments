2095071ebb528f8b2550429d7c85f04ecab90b1e
Shubham Bayas <shubham.bayas@joshsoftware.com>
2025-02-13 00:10:48 +0530

    Add Assignment 5

---
+++ Day5/q2.js
@@ -0,0 +1,18 @@
+// Implement a Fibonacci sequence generator using a generator function in
+// JavaScript.
+
+function* fibonacci() {
+    let [a, b] = [0, 1];
+    while (true) {
+      yield a;
+      [a, b] = [b, a + b];
+    }
+  }
+  
+  const fib = fibonacci();
+  console.log(fib.next().value); 
+  console.log(fib.next().value); 
+  console.log(fib.next().value); 
+  console.log(fib.next().value); 
+  console.log(fib.next().value); 
+  
